b254dd1ab3e179fc52544d4531c43ccac5560bbf
JiangJingzhe <contact@jiangjingzhe.com>
2026-06-17 05:42:57 +0800

    Initial Commit

---
+++ SSHocks/SSHocksApp.swift
@@ -0,0 +1,32 @@
+//
+//  SSHocksApp.swift
+//  SSHocks
+//
+//  Created by JIANGJINGZHE on 17/6/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SSHocksApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
